78de4e6613713675a5a0dbb62ae2c37913dbb030
Yeongseo Kim <dudtjakdl@naver.com>
2022-01-25 22:40:55 +0900

    주석 추가

--- Brute_Force/BOJ_1436_영화감독_숌.java
+++ Brute_Force/BOJ_1436_영화감독_숌.java
@@ -13,12 +13,13 @@ public class BOJ_1436_영화감독_숌 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int result = 666; // 결과값(N이 1이면 답은 666이므로 666으로 초기화)
+		int result = 666; // 결과값 (N이 1이면 답은 666이므로 666으로 초기화)
 
+		// N만큼 탐색
 		for (int i = 1; i < N; i++) {
-			int title = result;
+			int title = result; // 탐색을 시작해야하는 초기값을 title로 초기화
 
-			while (true) {
+			while (true) { // 연속된 666 문자열이 포함된 num을 찾을때까지 탐색값을 +1 하며 반복
 				String num = Integer.toString(++title);
 				if (num.contains("666")) {
 					result = title;
