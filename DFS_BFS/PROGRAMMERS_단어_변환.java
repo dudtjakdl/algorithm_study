782dd045e309a8e77a5ee67d441720ff6bcdea6b
Yeongseo Kim <dudtjakdl@naver.com>
2022-04-12 21:48:32 +0900

    코드 수정

--- DFS_BFS/PROGRAMMERS_단어_변환.java
+++ DFS_BFS/PROGRAMMERS_단어_변환.java
@@ -5,14 +5,13 @@ package DFS_BFS;
  * 걸린 시간 : 30분
 */
 class PROGRAMMERS_단어_변환 {
-    static String begin, target;
+    static String target;
     static String[] words;
     static int N, answer;
     static boolean[] visited;
     
     public int solution(String begin, String target, String[] words) {
         answer = Integer.MAX_VALUE; // 문제의 답 -> 가장 큰 수로 초기화
-        this.begin = begin; // 시작 단어
         this.target = target; // 타겟 단어
         this.words = words; // 단어집합
         N = words.length; // 단어집합의 단어 개수
@@ -27,14 +26,14 @@ class PROGRAMMERS_단어_변환 {
             if (i == N-1) return 0;
         }
         
-        bfs(begin, 0);
+        dfs(begin, 0);
         
         return answer;
     }
     
     // current: 현재 단어 cnt: 현재까지 거쳐온 단계
-    private static void bfs(String current, int cnt) {
-    	// 현재 단어가 target 단어이면 bfs탐색 종료
+    private static void dfs(String current, int cnt) {
+    	// 현재 단어가 target 단어이면 dfs탐색 종료
         if (current.equals(target)) {
         	// 현재까지 거쳐온 단계가 최소값이면 갱신
             answer = Math.min(answer, cnt);
@@ -46,7 +45,7 @@ class PROGRAMMERS_단어_변환 {
         	// 아직 방문하지 않은 단어이고 변환가능할 경우
             if (!visited[i] && isConvertible(current, words[i])) {
                 visited[i] = true; // 방문체크
-                bfs(words[i], cnt+1); // 다음 bfs 탐색으로 이동
+                dfs(words[i], cnt+1); // 다음 dfs 탐색으로 이동
                 visited[i] = false; // 방문체크 해제
             }
         }
